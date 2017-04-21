69e3879f26f9410e5eb02461064db7fd94298726
GitHubLaboratory <DuMaHism@yandex.ru>
2017-04-21 23:51:08 +0700

    init

---
+++ src/class_test.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   class_test.cpp
+ * Author: Admin
+ * 
+ * Created on 21 апреля 2017 г., 23:48
+ */
+
+#include "class_test.h"
+
+class_test::class_test() {
+    
+}
+
+class_test::class_test(const class_test& orig) {
+}
+
+class_test::~class_test() {
+}
+
---
+++ src/class_test.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   class_test.h
+ * Author: Admin
+ *
+ * Created on 21 апреля 2017 г., 23:48
+ */
+
+#ifndef CLASS_TEST_H
+#define CLASS_TEST_H
+
+class class_test {
+public:
+    class_test();
+    class_test(const class_test& orig);
+    virtual ~class_test();
+private:
+
+};
+
+#endif /* CLASS_TEST_H */
+
