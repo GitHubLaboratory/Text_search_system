cca4d8a4e4acf34ba7c50491fec8afd944f80799
nql98 <baganikitanb@gmail.com>
2017-05-16 00:07:45 +0700

    Prepare for testing functions

--- main.cpp
+++ main.cpp
@@ -11,29 +11,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-
-    SearchEngine searchEngined;
-    std::vector<FindFile> findfiles;
-
-    std::string path;
-
-    std::string str;
-
-
-    searchEngined.getStringF("C:/log.txt", 5, str);
-
-    std::cout<<str;
-
-    std::cin>>path;
-    searchEngined.searchbyLine(path, "USB", findfiles);
-    for(int i = 0; i < findfiles.size(); i++)
-    {
-        std::cout<< findfiles.at(i).nameFile<<std::endl;
-        for(int j = 0; j < findfiles.at(i).lineNumbers.size(); j++)
-            std::cout<< findfiles.at(i).lineNumbers.at(j)<<"   ";
-        std::cout<< std::endl;
-    }
-
-
     return a.exec();
 }
---
+++ test/searchengine_test.cpp
@@ -0,0 +1,60 @@
+#include "ctest.h"
+#include "../src/SearchEngine.h"
+#include <iostream>
+
+//SearchEngine se;
+char path[] = "C:/NVIDIA/Text_search_system/test/test.txt";
+
+
+//CTEST(validationExtensionf, validationExtensionf_test)
+//{
+//    int expected = 1;
+//    int result = se.validationExtensionf("asd.txt", "txt");
+//    ASSERT_EQUAL(expected, result);
+//}
+
+//CTEST(concat, concat_test)
+//{
+//    char *s1, *s2;
+//    s1 = new char [3];
+//    s2 = new char [4];
+//    s1 = "abc";
+//    s2 = "defg";
+//    char *exp  = new char [7];
+//    exp = "abcdefg";
+//    char *real = se.concat(s1,s2);
+//    ASSERT_STR(exp, real);
+//}
+
+//CTEST(getStringF, getStringF_test)
+//{
+//    std::string exp = "/cast [@focus] парализующий удар\n";
+//    std::string result;
+//    se.getStringF(path, 2, result);
+//    ASSERT_STR(exp.c_str(), result.c_str());
+//}
+
+//CTEST(fileSearch, fileSearch_test)
+//{
+//    std::vector<int> vect;
+//    se.fileSearch(path,"MACRO", vect);
+//    std::vector<int> exp {0,4,8,12,15};
+//    int result = 1;
+//    int size = exp.size();
+//    if (size == vect.size())
+//    {
+//        for(int i = 0; i < size; i++)
+//        {
+//            if(exp.at(i) != vect.at(i))
+//            {
+//                result = 0;
+//                break;
+//            }
+//        }
+//    }
+//    else
+//        result = 0;
+//    ASSERT_EQUAL(result, 1);
+//}
+
+
